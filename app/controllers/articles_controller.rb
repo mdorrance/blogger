ae4824bce735f17395ccfc16f32e95c69aae2f35
Michael Dorrance <dorrancemd@gmail.com>
2015-07-22 14:09:51 -0600

    first blogger commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,28 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def create
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
